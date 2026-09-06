95d9b502fa52eeb652e14de762af42f468ac0cc8
Deekshi-3-7 <deekshidt18@gmail.com>
2026-09-06 18:20:17 +0530

    fix: support route refresh on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useEffect } from 'react'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+function RedirectHandler() {
+  useEffect(() => {
+    const params = new URLSearchParams(
+      window.location.search
+    )
+
+    const redirect = params.get('redirect')
+
+    if (!redirect) {
+      return
+    }
+
+    const decodedPath =
+      decodeURIComponent(redirect)
+
+    window.history.replaceState(
+      null,
+      '',
+      `/microread${decodedPath}`
+    )
+  }, [])
+
+  return <App />
+}
+
+createRoot(
+  document.getElementById('root')!
+).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RedirectHandler />
+  </StrictMode>
 )
